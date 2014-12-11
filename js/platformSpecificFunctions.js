e3d206e86bdb8a0f628085c06e778626184565a1
Pragna Gopa <pragna.gopa@hotmail.com>
2014-12-11 15:47:21 -0800

    Updates

--- js/platformSpecificFunctions.js
+++ js/platformSpecificFunctions.js
@@ -5,34 +5,27 @@
 function createPlatformSpecificFunctions() {
 
     var alertFunction;
-    if (typeof alert === 'undefined') {
-        alertFunction = function (text, done) {
-            var dialog = new Windows.UI.Popups.MessageDialog(text);
-            dialog.showAsync().done(function () {
-                if (typeof done === 'function') {
-                    done();
-                }
-            });
+
+    alertFunction = function (text, done) {
+        window.alert(text);
+        if (done) {
+            done();
         }
     }
 
-    var saveAppInfo = function (lastAppUrl, lastAppKey, lastUploadLogUrl) {
+    var saveAppInfo = function (lastAppUrl, lastAppKey) {
         /// <param name="lastAppUrl" type="String">The last value used in the application URL text box</param>
-        /// <param name="lastAppKey" type="String">The last value used in the application key text box</param>
-        /// <param name="lastUploadLogUrl" type="String">The last value used in the upload logs URL text box</param>
+        /// <param name="lastAppKey" type="String">The last value used in the application key text box</param>        
         var state = {
             lastAppUrl: lastAppUrl,
-            lastAppKey: lastAppKey,
-            lastUploadUrl: lastUploadLogUrl
+            lastAppKey: lastAppKey,            
         };
-
-        WinJS.Application.local.writeText('savedAppInfo.txt', JSON.stringify(state));
     }
 
     return {
         alert: alertFunction,
         saveAppInfo: saveAppInfo,
-        IsHTMLApplication: false,
+        IsHTMLApplication: true,
     };
 }
 
